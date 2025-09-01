3a151281df158d62dccd56291052e7c332cd95dc
mm.dev <maildemartin@gmail.com>
2025-09-01 00:19:07 -0300

    Restaurar aplicación principal

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
